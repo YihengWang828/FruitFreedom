e32c69ed2edce0a43e60b84bda81ad49414949ce
edPanda <y1433011488@outlook.com>
2020-06-24 14:30:30 +0800

    推荐页面测试成了

--- FruitFree/flaskr/recommendation.py
+++ FruitFree/flaskr/recommendation.py
@@ -12,7 +12,7 @@ bp=Blueprint('recom',__name__,url_prefix='/recom')
 def recom():
     conn=db.get_db()
     cursor=conn.cursor()
-
+    '''
     #测试一下
     type_="水果"
     sql="select * from all_category where category = '水果' order by marks desc limit 10 ;"
@@ -35,3 +35,34 @@ def recom():
             }
         )
     print(data)
+    '''
+    if request.method=='GET':
+        return render_template('tuijian.html')
+    elif request.method=='POST':
+        a=request.get_json(force=True)
+        print(a)
+        type_=a['name']
+        
+        sql="select * from all_category where category = '"+type_+"' order by marks desc limit 10 ;"
+        print(sql)
+        cursor.execute(sql)
+        results=cursor.fetchall()
+        print(results)
+        data=[]
+        for re in results:
+            data.append(
+                {
+                    "category":re[0],
+                    "brand":re[1],
+                    "price":re[2],
+                    "sales":re[3],
+                    "delivermark":re[4],
+                    "describemark":re[5],
+                    "servemark":re[6],
+                    "picture":re[8]
+                    }
+                )
+        print(data)
+        return jsonify({
+            "data":data
+        })
